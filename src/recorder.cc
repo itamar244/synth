abbe5f643f6b09be3de7aa8487d823b78c4221b3
itamar244 <itamary24@gmail.com>
2018-05-21 16:20:08 +0300

    feat(Recorder): make the recorder bloody work

--- src/recorder.cc
+++ src/recorder.cc
@@ -5,81 +5,82 @@ namespace synth {
 
 namespace {
 
-inline uint8_t MillisScale(uint32_t milliseconds) {
-	return milliseconds / 16 % 255;
+inline uint32_t MillisScale(uint32_t milliseconds) {
+	return milliseconds / 16;
 }
 
 uint16_t GetSongStartFromPos(uint8_t song_pos) {
 	uint16_t cur_song_pos = 0, pos = 0;
 
-	while (song_pos > cur_song_pos) {
-		// adding phrase's tones size and
-		// the bytes representings amount of tones and length
-		pos += store::Get(pos) + 2;
+	while (cur_song_pos < song_pos && pos < store::Size() - 1) {
 		if (store::Get(pos) == 0) {
 			++cur_song_pos;
+			++pos;
+		} else {
+			// adding phrase's tones size and
+			// the bytes representings amount of tones and length
+			pos += store::Get(pos + 1) + 2;
 		}
-		++pos;
 	}
 
 	return pos;
 }
 
-inline Phrase GetPhraseFromEEPROM(uint16_t pos) {
-	uint32_t tones_length = store::Get(pos);
-	Phrase::Tones tones;
-	tones.reserve(tones_length);
-
-	for (uint16_t i = 0; i < tones_length; i++) {
-		tones.push_back(store::Get(pos + i + 1));
-	}
-
-	return {tones, store::Get(pos + tones_length + 1)};
-}
-
 } // namespace
 
-void Recorder::PushTones(const Audio::ToneList& tones) {
-	store::Push(tones.size());
+Recorder::~Recorder() {
+	// terminate the song record
+	store::Push(0);
+}
 
+void Recorder::PushTones(const Audio::ToneList& tones) {
 	uint32_t cur_millis = millis();
+
+	store::Push(MillisScale(cur_millis - prev_millis_));
+	store::Push(tones.size());
 
 	for (auto& tone : tones) {
 		store::Push(tone);
 	}
 
-	store::Push(MillisScale(cur_millis - prev_millis_));
+
 	prev_millis_ = cur_millis;
 }
 
 RecordsPlayer::RecordsPlayer(uint16_t song_pos)
-	: pos_(GetSongStartFromPos(song_pos))
-	, prev_millis_(millis())
-	, inited_(false)
-	, cur_phrase_(GetPhraseFromEEPROM(pos_)) {}
+		: pos_(GetSongStartFromPos(song_pos)) {}
 
-bool RecordsPlayer::Tick(Audio* audio) {
-	if (!inited_) {
-		inited_ = true;
-		audio->AddTones(cur_phrase_.tones);
+void RecordsPlayer::ReadCurrentPhrase() {
+	uint8_t tones_size = store::Get(pos_ + 1);
+	Phrase::Tones tones(tones_size);
+
+	for (uint16_t i = 0; i < tones_size; i++) {
+		tones[i] = store::Get(pos_ + i + 2);
+	}
+
+	cur_phrase_ = {tones, store::Get(pos_)};
+}
+
+bool RecordsPlayer::Play(Audio* audio) {
+		if (Finished()) {
+			return false;
+		}
+		if (!started_) {
+			ReadCurrentPhrase();
+			audio->AddTones(cur_phrase_.tones);
+			prev_millis_ = millis();
+			started_ = true;
+		} else if (MillisScale(millis() - prev_millis_) >= cur_phrase_.length) {
+			audio->RemoveTones(cur_phrase_.tones);
+			pos_ += cur_phrase_.tones.size() + 2;
+			if (!Finished()) {
+				ReadCurrentPhrase();
+				audio->AddTones(cur_phrase_.tones);
+				prev_millis_ = millis();
+			}
+		}
 		return true;
 	}
 
-	uint32_t cur_millis = millis();
-
-	if (MillisScale(cur_millis - prev_millis_) >= cur_phrase_.length) {
-		audio->RemoveTones(cur_phrase_.tones);
-		pos_ += cur_phrase_.length + 2;
-
-		// checking if song ended
-		if (pos_ == store::Size() || store::Get(pos_) == 0) {
-			return false;
-		}
-		cur_phrase_ = GetPhraseFromEEPROM(pos_);
-		audio->AddTones(cur_phrase_.tones);
-	}
-
-	return true;
-}
 
 } // namespace synth
--- src/recorder.h
+++ src/recorder.h
@@ -1,14 +1,29 @@
+// Recorder and RecordsPlayer implementation.
+// Recorder pushes melodies into memory and RecordsPlayer reads and
+// plays melodies by a given index.
+// Both classes use the same data format of melodies and read and write
+// the melodies from the store.
+//
+// The data format:
+// - PHRASE = [LENGTH, SIZE, ...TONES]
+// - MELODY = [...PHRASES, 0]
+// - STORE  = [...SONGS]
+//
+// Each phrase made from length in `milliseconds / 16`, size of tones, and tone values.
+// Each melody consists from many phrases, ending with zero
 #pragma once
 
 #include <Arduino.h>
 #include <stdint.h>
 #include "audio.h"
 #include "phrase.h"
+#include "store.h"
 
 namespace synth {
 
 class Recorder {
 public:
+	~Recorder();
 	void PushTones(const Audio::ToneList& tones);
 
 private:
@@ -18,12 +33,19 @@ private:
 class RecordsPlayer {
 public:
 	RecordsPlayer(uint16_t song_pos);
-	bool Tick(Audio* audio);
+	bool Play(Audio* audio);
+
 
 private:
+	bool started_ = false;
 	uint32_t pos_, prev_millis_ = millis();
-	bool inited_;
 	Phrase cur_phrase_;
+
+	bool Finished() const {
+		return pos_ == store::Size() || store::Get(pos_) == 0;
+	}
+
+	void ReadCurrentPhrase();
 };
 
 } // namespace synth
