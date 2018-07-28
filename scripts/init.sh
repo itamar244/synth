08352ac3593b0f455ea3dc636f0b8d7911feb58e
itamar244 <itamary24@gmail.com>
2018-07-28 19:57:27 +0300

    add boost_filesystem

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,3 +1,8 @@
+# installing boost-filesystem if it doesn't exist
+if ! ldconfig -p | grep -c boost_filesystem > /dev/null; then
+	sudo apt-get install libboost-filesystem-dev
+fi
+
 if [ ! -d "build" ]; then
 	mkdir build
 fi
