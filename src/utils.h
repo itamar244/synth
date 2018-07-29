2d26e95987a99dfe938a93ab37151e548c050392
itamar244 <itamary24@gmail.com>
2018-07-29 16:03:18 +0300

    update utils

--- src/utils.h
+++ src/utils.h
@@ -21,6 +21,38 @@ inline bool HasItem(const Iterable& iterable, const V& item) {
 	return FindItem(iterable, item) != iterable.end();
 }
 
+template<class Ptr>
+inline void SetPtr(Ptr& ptr, const Ptr& value) {
+	if (ptr != nullptr) {
+		delete ptr;
+	}
+	ptr = value;
+}
+
+// A normalized `delete` operator that also set the pointer to nullptr,
+// because `delete` only deallocate memory.
+template<class Ptr>
+inline void DeletePtr(Ptr& ptr) {
+	delete ptr;
+	ptr = nullptr;
+}
+
+template<class Ptr>
+inline void MaybeDeletePtr(Ptr& ptr) {
+	if (ptr != nullptr) {
+		DeletePtr(ptr);
+	}
+}
+
+template<class Iterator>
+auto Advance(Iterator it, unsigned n) -> decltype(*it) {
+	for (unsigned i = 0; i < n; i++) {
+		++it;
+	}
+
+	return *it;
+}
+
 template<class Lhs, class Rhs>
 inline bool Is(const Lhs& lhs, const Rhs& rhs) {
 	return lhs == rhs;
